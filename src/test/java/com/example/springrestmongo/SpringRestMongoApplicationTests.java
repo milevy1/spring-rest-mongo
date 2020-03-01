69af73204d14e8df224cd28b5582b7c423166b39
Matt Levy <milevy1@gmail.com>
2020-03-01 16:49:51 -0600

    Initial commit

---
+++ src/test/java/com/example/springrestmongo/SpringRestMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
